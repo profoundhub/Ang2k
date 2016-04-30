1b7bdead6b67daf61294b40e8e9960822194b34c
Daniel Lim <hbt@markhamboards.com>
2016-04-29 20:50:33 -0400

    update cide

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,18 @@
 import {Component} from 'angular2/core';
 
+import {ProductListComponent} from './products/product-list.component';
+
 @Component({
     selector: 'pm-app',
     template: `
         <div>
             <h1>{{pageTitle}}</h1>
-            <div>My First Component</div>
-        </div> `
+            <pm-products>111</pm-products>
+        </div> 
+        `,
+    directives: [ProductListComponent] 
+    
 })
-
 export class AppComponent {    
     pageTitle: string = "Profound Ideation Management";
 }
